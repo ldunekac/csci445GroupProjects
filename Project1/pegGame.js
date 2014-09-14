48f2b6a887fca6b2a150bd10f2c2a9aa102e7178
Luke Dunekacke <ldunekac@mymail.mines.edu>
2014-09-13 18:44:03 -0600

    added more js

--- Project1/pegGame.js
+++ Project1/pegGame.js
@@ -22,12 +22,18 @@ function drawBoard()
 	var c = document.getElementById('boardCanvas');
 	var ctx = c.getContext('2d');
 	for (var i = 0; i < board.length; i++) {
-		
-		if(board[i].pegIn)
+		if(board[i].inQuestion) 
+		{
 			ctx.fillStyle = 'blue';
+		}
+		else if(board[i].pegIn)
+		{
+			ctx.fillStyle = 'yellow';
+		}
 		else
+		{
 			ctx.fillStyle = 'green';
-		
+		}
 		ctx.beginPath();
 		ctx.arc(board[i].x,board[i].y,board[i].radious,0,2*Math.PI,false);
 		ctx.fill();
@@ -43,7 +49,7 @@ function makeBoard()
 		for(var j = 0; j < i + 1; j++)
 		{
 			var spacing = c.width / (i+2);
-			var peg = new Peg(spacing*j + spacing,100*(i+1),i);
+			var peg = new Peg(spacing*j + spacing,100*(i+1),i,j);
 			if(j==0)
 				peg.pegIn = false;
 			board.push(peg);
@@ -52,17 +58,76 @@ function makeBoard()
 }
 
 
-function Peg(x,y,rowID)
+function Peg(x, y, rowID, columnID)
 {
 	this.x = x;
 	this.y = y;
 	this.radious = 10;
-	this.rowID = rowID;
+	this.row = rowID;
+	this.column = columnID;
 	this.pegIn = true;
+	this.inQuestion = false;
 }
 
 function showMoves(e)
 {
-	var xpos = e.clientX;
-	var ypos = y.clientY;
+	var x = e.clientX - this.offsetLeft;
+	var y = e.clientY - this.offsetTop;
+	for(var i = 0; i < 15; i++)
+	{
+		if((x-board[i].x)*(x-board[i].x) + (y-board[i].y)*(y-board[i].y) < board[i].radious*board[i].radious)
+		{
+			findValidMoves(i);
+		}
+	}
+	drawBoard();
+}
+
+function findValidMoves(boardPosition)
+{
+	// check row
+	var farLeft = board[boardPosition].column-2;
+	var farRight = board[boardPosition].column+2;
+	var left = board[boardPosition].column-1;
+	var right = board[boardPosition].column+1;
+	var farRightEmpty = -1;
+	var farLeftEmpty = -1;
+	var rightFull = -1;
+	var leftFull = -1;
+	for(var i = 0; i < board.length; i++)
+	{
+		if(board[i].row == board[boardPosition].row)
+		{
+			if(board[i].column == farLeft && !board[i].pegIn)
+			{
+				farLeftEmpty = i;
+			}
+			else if(board[i].column == farRight && !board[i].pegIn)
+			{
+				farRightEmpty = i;
+			}
+			else if(board[i].column == left && board[i].pegIn)
+			{
+				rightFull = i;
+			}
+			else if(board[i].column == right && board[i].pegIn)
+			{
+				leftFull = i;
+			}
+		}
+	}
+	if(farRightEmpty != -1 && rightFull != -1)
+	{
+		board[farRightEmpty].inQuestion = true;
+	}
+	if(farLeftEmpty != -1 && farLeft != -1)
+	{
+		board[farRightEmpty].inQuestion = true;
+	}
+
+
+
+
+	// check column aka /
+	// check diagonal aka \
 }
